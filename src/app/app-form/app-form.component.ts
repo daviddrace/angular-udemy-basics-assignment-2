2f99420387234aa978c89971d00024bc6f001780
David Drace <ddrace@Davids-MacBook-Pro.local>
2017-10-27 11:44:24 -0500

    assignment complete.

---
+++ src/app/app-form/app-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './app-form.component.html',
+  styleUrls: ['./app-form.component.css']
+})
+
+export class FormComponent implements OnInit {
+
+  userName:string = "";
+
+  contructor() {
+
+  }
+
+  ngOnInit() { }
+
+  resetUserName(){
+    this.userName = "";
+  }
+
+}
